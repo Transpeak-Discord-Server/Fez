d8b1c911b800f9a5c624cdf27092844595cafc91
Aven <a@avenf.com>
2026-03-22 03:34:42 +0000

    fixed bugs in Ash's and Zoey's staff commands

--- Fez/cogs/staff_commands.py
+++ Fez/cogs/staff_commands.py
@@ -33,25 +33,26 @@ class StaffCommands(commands.Cog):
 
     # Ash
     ASH_IMAGE_PATH = os.path.join(PROJECT_PATH, 'assets/images/ash_staff_command.gif')
-    with open(ASH_IMAGE_PATH, 'rb') as f:
-        ASH_IMAGE=discord.File(f, filename=ASH_IMAGE_PATH)
 
     @commands.command()
     async def ash(self, ctx: commands.Context):
-        return await ctx.reply(file=self.ASH_IMAGE)
+        with open(self.ASH_IMAGE_PATH, 'rb') as f:
+            return await ctx.reply(file=discord.File(f, self.ASH_IMAGE_PATH))
 
 
     # Zoey
     @commands.command()
-    async def zoey(self, ctx: commands.Context, member: discord.Member = None):
-        chosen_obj_1, chosen_obj_2 = sample(ZOEY_DATA.ZOEY_OBJECTS, 2)
+    async def zoey(self, ctx: commands.Context, member = None):
+        if type(member) != discord.Member:
+            member = None
+        chosen_obj_1, chosen_obj_2 = sample(ZOEY_DATA['ZOEY_OBJECTS'], 2)
         return await ctx.reply(f"Hello{" " + member.display_name if member else ""}, this is Fez from Transpeak's "
-                               f"{choice(ZOEY_DATA.ZOEY_JOB_TITLES)}. I'm here to inform you that we have decided to banish "
-                               f"you to {choice(ZOEY_DATA.ZOEY_LOCATIONS)}. {choice(ZOEY_DATA.ZOEY_REACTION)} you have a chance at "
-                               f"redemption by finding {choice(ZOEY_DATA.ZOEY_STRUCTURES)} and collecting "
+                               f"{choice(ZOEY_DATA['ZOEY_JOB_TITLES'])}. I'm here to inform you that we have decided to banish "
+                               f"you to {choice(ZOEY_DATA['ZOEY_LOCATIONS'])}. {choice(ZOEY_DATA['ZOEY_REACTION'])} you have a chance at "
+                               f"redemption by finding {choice(ZOEY_DATA['ZOEY_STRUCTURES'])} and collecting "
                                f"{randint(0,1000)} {chosen_obj_1} and {randint(0,1000)} {chosen_obj_2}. If "
                                f"you fail to do so within {randint(1,7)} days we will have no choice but to report "
-                               f"you for {choice(ZOEY_DATA.ZOEY_CRIME)}. Thank you for your time.")
+                               f"you for {choice(ZOEY_DATA['ZOEY_CRIME'])}. Thank you for your time.")
 
 
 async def setup(bot):
