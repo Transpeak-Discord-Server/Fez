1a9347bc4dc1e0bbed5656c158b25fe159d0888c
Aven <a@avenf.com>
2026-03-23 11:38:02 +0000

    refactored database connection and message count manager

--- shared/database/msgcount_manager.py
+++ shared/database/msgcount_manager.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from core import Database
 
 class MsgCountManager:
@@ -6,15 +7,49 @@ class MsgCountManager:
         self.msgcount_db = Database("message_count.db")
         self.msgcount_week_db = Database("message_count_week.db")
 
+    @staticmethod
+    def getweek():
+        a = datetime(2017, 7, 31, 00, 00, 00)
+        b = datetime.now()
+        return str(int((b - a).total_seconds() / (7 * 24 * 60 * 60)))
+
+    async def initialise(self):
+        await self.msgcount_db.connect()
+        await self.msgcount_db.connect()
+
     async def get_msg_count(self, user_id: int, week: bool = False) -> int:
         if week:
-            cursor = await self.msgcount_week_db.execute("SELECT count FROM msgcount_week WHERE id = ?", (str(user_id),))
-            return cursor.fetchone()[0]
-        cursor = await self.msgcount_db.execute("SELECT count FROM msgcount WHERE id = ?", (str(user_id),))
-        return cursor.fetchone()[0]
+            db = self.msgcount_week_db
+            query = "SELECT count FROM msgcount_week WHERE id = ? and week = ?"
+            args = (str(user_id),self.getweek())
+        else:
+            db = self.msgcount_db
+            query = "SELECT count FROM msgcount WHERE id = ?"
+            args = (str(user_id),)
+        row = await db.fetchone(db.database_name, query, *args)
+        return row['count'] if row else 0
 
     async def increment_msg_count(self, user_id: int):
-        await self.msgcount_db.execute("UPDATE msgcount SET count = count + 1 WHERE id = ?", (str(user_id),))
-        await self.msgcount_db.commit()
-        await self.msgcount_week_db.execute("UPDATE msgcount_week SET count = count + 1 WHERE id = ?", (str(user_id),))
-        await self.msgcount_week_db.commit()
+        week = self.getweek()
+        user = str(user_id)
+
+        await self.msgcount_db.execute(self.msgcount_db.database_name,
+            "INSERT INTO msgcount (id, count) VALUES (?, ?) ON CONFLICT(id) DO UPDATE SET count = count + 1",
+            (user, 1)
+        )
+
+        await self.msgcount_week_db.execute(self.msgcount_week_db.database_name,
+            "INSERT INTO msgcount_week (id, count, week) VALUES (?, ?, ?) ON CONFLICT(id, week) DO UPDATE SET count = count + 1",
+            (user, 1, week)
+        )
+
+    async def set_msg_count(self, user_id: int, count: int):
+        await self.msgcount_db.execute(self.msgcount_db.database_name,
+            "UPDATE msgcount SET count = ? WHERE id = ?",
+            (count, str(user_id))
+        )
+        await self.msgcount_week_db.execute(self.msgcount_week_db.database_name,
+            "UPDATE msgcount_week SET count = ? WHERE id = ?",
+            (count, str(user_id))
+        )
+
