47a0df0fd818fe764d34359463bf810e4f29dc44
Aven <a@avenf.com>
2026-03-23 16:06:11 +0000

    added ban database manager

---
+++ shared/database/ban_manager.py
@@ -0,0 +1,26 @@
+from time import time
+from typing import Any
+
+from core import Database
+
+class BanManager:
+
+    def __init__(self):
+        self.ban_db = Database("bans.db")
+
+    async def initialise(self):
+        await self.ban_db.connect()
+
+    async def get_ban(self, user_id: int) -> dict[str, Any]:
+        row = await self.ban_db.fetchone("SELECT * FROM bans WHERE userid = ?", (str(user_id),))
+        return {
+            "userid": row['userid'],
+            "banned_by": row['banner'],
+            "timestamp": row['timestamp'],
+            "reason": row['reason']
+        }
+
+    async def ban(self, user_id: int, banned_by: int, reason: str):
+        await self.ban_db.execute(
+            "INSERT INTO bans (userid, banner, timestamp, reason) VALUES (?, ?, ?)",
+            (str(user_id), str(banned_by), int(round(time() * 1000)), reason))
