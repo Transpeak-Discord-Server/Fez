dd6d671ea1351e3f4bc6d02036ae190cef63dab4
Aven <a@avenf.com>
2026-03-23 11:38:02 +0000

    refactored database connection and message count manager

--- shared/database/core.py
+++ shared/database/core.py
@@ -1,3 +1,6 @@
+from sqlite3 import Row
+from typing import Iterable
+
 import aiosqlite
 import os
 
@@ -12,15 +15,33 @@ class Database:
 
     async def __aenter__(self):
         self.connection = await aiosqlite.connect(self.database_path)
+        self.connection.row_factory = aiosqlite.Row
         return self
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
         if self.connection:
+            if exc_type is None:
+                await self.connection.commit()
             await self.connection.close()
 
-    async def execute(self, query: str, *args) -> aiosqlite.Cursor:
-        cursor = await self.connection.execute(query, *args)
-        return cursor
+    async def connect(self):
+        await self.__aenter__()
 
-    async def commit(self):
-        await self.connection.commit()
+    async def disconnect(self):
+        await self.__aexit__(None, None, None)
+
+    # Should be used for INSERT, UPDATE, DELETE, etc.
+    async def execute(self, query: str, *args) -> int:
+        async with self.connection.execute(query, *args) as cursor:
+            await self.connection.commit()
+            return cursor.rowcount
+
+    # Should be used for SELECT
+    async def fetchall(self, query: str, *args) -> Iterable[Row]:
+        async with self.connection.execute(query, *args) as cursor:
+            return await cursor.fetchall()
+
+    # Should be used for SELECT
+    async def fetchone(self, query: str, *args) -> Row:
+        async with self.connection.execute(query, *args) as cursor:
+            return await cursor.fetchone()
