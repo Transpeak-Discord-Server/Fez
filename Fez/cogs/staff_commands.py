742696f1ab9a3a6fcec130eba098949bea0db209
Aven <a@avenf.com>
2026-04-15 18:13:32 +0100

    Added mypy dependencies and static typing to satisfy mypy

--- Fez/cogs/staff_commands.py
+++ Fez/cogs/staff_commands.py
@@ -1,85 +1,104 @@
+from typing import Any
+
 import discord
 import os
 import pytz
 from discord.ext import commands
 from datetime import datetime
 from random import randint, choice, sample
+
+from discord.ext.commands import Context
+
 from shared.config import Config
 
 PROJECT_PATH = os.path.join(os.path.dirname(__file__), '..')
 
 class StaffCommands(commands.Cog):
 
-    def __init__(self, bot):
+    def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
+
+    def cog_check(self, ctx: Context[Any]) -> bool:
+        return ctx.guild is not None
 
     # Aven
     AVEN_USER_ID = 275263004961144833
     AVEN_SLEEP_WINDOW = (2, 8)
 
     @commands.group(invoke_without_command=True)
-    async def aven(self, ctx: commands.Context):
-        return await ctx.reply(f"<@{self.AVEN_USER_ID}>, boop!")
+    async def aven(self, ctx: commands.Context[Any]) -> None:
+        await ctx.reply(f"<@{self.AVEN_USER_ID}>, boop!")
+        return None
 
-    @aven.command(name='time')
-    async def aven_time(self, ctx: commands.Context):
+    @aven.command(name='time') # type: ignore
+    async def aven_time(self, ctx: commands.Context[Any]) -> None:
         time_uk = datetime.now(pytz.timezone('Europe/London'))
         time_uk_str = time_uk.strftime('%H:%M:%S')
         start, end = self.AVEN_SLEEP_WINDOW
         if end >= time_uk.hour >= start:
-            return await ctx.reply(f"The time for Aven is {time_uk_str}. She's probably asleep!")
-        return await ctx.reply(f"The time for Aven is {time_uk_str}")
+            await ctx.reply(f"The time for Aven is {time_uk_str}. She's probably asleep!")
+            return None
+        await ctx.reply(f"The time for Aven is {time_uk_str}")
+        return None
 
 
     # Ash
     ASH_IMAGE_PATH = os.path.join(PROJECT_PATH, 'assets/images/ash_staff_command.gif')
 
     @commands.command()
-    async def ash(self, ctx: commands.Context):
+    async def ash(self, ctx: commands.Context[Any]) -> None:
         with open(self.ASH_IMAGE_PATH, 'rb') as f:
-            return await ctx.reply(file=discord.File(f, self.ASH_IMAGE_PATH))
-
+            await ctx.reply(file=discord.File(f, self.ASH_IMAGE_PATH))
+            return None
 
     # Zoey
     ZOEY_DATA = Config.json_config['ZOEY_DATA']
 
     @commands.command()
-    async def zoey(self, ctx: commands.Context, member = None):
-        if member:
-            member = int(member) if member.isdigit() else None
-            member = ctx.guild.get_member(member)
+    async def zoey(self, ctx: commands.Context[Any], member_str: str | None = None) -> None:
+        member_int = int(member_str) if member_str and member_str.isdigit() else None
+        if not member_int:
+            return None
+        assert ctx.guild is not None
+        member = ctx.guild.get_member(member_int)
         chosen_obj_1, chosen_obj_2 = sample(self.ZOEY_DATA['ZOEY_OBJECTS'], 2)
-        return await ctx.reply(f"Hello{" " + member.display_name if type(member) == discord.Member else ""}, this is Fez from Transpeak's "
-                               f"{choice(self.ZOEY_DATA['ZOEY_JOB_TITLES'])}. I'm here to inform you that we have decided to banish "
-                               f"you to {choice(self.ZOEY_DATA['ZOEY_LOCATIONS'])}. {choice(self.ZOEY_DATA['ZOEY_REACTION'])} you have a chance at "
-                               f"redemption by finding {choice(self.ZOEY_DATA['ZOEY_STRUCTURES'])} and collecting "
-                               f"{randint(0,1000)} {chosen_obj_1} and {randint(0,1000)} {chosen_obj_2}. If "
-                               f"you fail to do so within {randint(1,7)} days we will have no choice but to report "
-                               f"you for {choice(self.ZOEY_DATA['ZOEY_CRIME'])}. Thank you for your time.")
+        await ctx.reply(f"Hello{" " + member.display_name if type(member) == discord.Member else ""}, this is Fez from Transpeak's "
+           f"{choice(self.ZOEY_DATA['ZOEY_JOB_TITLES'])}. I'm here to inform you that we have decided to banish "
+           f"you to {choice(self.ZOEY_DATA['ZOEY_LOCATIONS'])}. {choice(self.ZOEY_DATA['ZOEY_REACTION'])} you have a chance at "
+           f"redemption by finding {choice(self.ZOEY_DATA['ZOEY_STRUCTURES'])} and collecting "
+           f"{randint(0,1000)} {chosen_obj_1} and {randint(0,1000)} {chosen_obj_2}. If "
+           f"you fail to do so within {randint(1,7)} days we will have no choice but to report "
+           f"you for {choice(self.ZOEY_DATA['ZOEY_CRIME'])}. Thank you for your time.")
+        return None
 
     # Cat
     @commands.command()
-    async def cat(self, ctx: commands.Context):
-        return await ctx.reply("https://tenor.com/view/15447330")
+    async def cat(self, ctx: commands.Context[Any]) -> None:
+        await ctx.reply("https://tenor.com/view/15447330")
+        return None
 
     # Luna
     @commands.command()
-    async def luna(self, ctx: commands.Context):
+    async def luna(self, ctx: commands.Context[Any]) -> None:
         with open(os.path.join(PROJECT_PATH, 'assets/images/luna_staff_command.png'), 'rb') as f:
-            return await ctx.reply(file=discord.File(f, 'luna_staff_command.png'))
+            await ctx.reply(file=discord.File(f, 'luna_staff_command.png'))
+            return None
 
     # Simon
     SIMON_USER_ID = 1383285080700747837
 
     @commands.command()
-    async def simon(self, ctx: commands.Context):
-        return await ctx.reply(f"<@{1383285080700747837}> woof!")
+    async def simon(self, ctx: commands.Context[Any]) -> None:
+        await ctx.reply(f"<@{1383285080700747837}> woof!")
+        return None
 
     # Ren
     @commands.command()
-    async def ren(self, ctx: commands.Context):
-        return await ctx.reply("yo")
+    async def ren(self, ctx: commands.Context[Any]) -> None:
+        await ctx.reply("yo")
+        return None
 
 
-async def setup(bot):
+async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(StaffCommands(bot))
+    return None
--- shared/utils/misc.py
+++ shared/utils/misc.py
@@ -1,7 +1,13 @@
 from functools import singledispatch
 from datetime import datetime
-from typing import Iterable, Protocol
+from typing import Iterable, Protocol, Any
 
+from discord.ext import commands
+
+from shared.utils.permissions import UserPermissionsError, Level
+
+def __init__():
+    pass
 
 class HasId(Protocol):
     id: int
@@ -13,9 +19,22 @@ def get_ids(items: Iterable[HasId]) -> list[int]:
 # format time
 
 @singledispatch
-def format_time(time, time_format: str = "t") -> str:
+def format_time(time: Any, time_format: str = "t") -> str:
     return f"<t:{int(time)}:{time_format}>"
 
 @format_time.register(datetime)
 def _(time: datetime, time_format: str = "t") -> str:
     return format_time(int(time.timestamp()), time_format)
+
+# Handle error
+
+async def shared_error(ctx: commands.Context[Any], error: Exception):
+    if isinstance(error, commands.CommandNotFound):
+        return None
+    if isinstance(error, UserPermissionsError):
+        if error.required_perms == Level.REGISTERED:
+            await ctx.reply("You need to be registered to do that! Please ping a staff member for help.")
+            return None
+        await ctx.reply(f"You need to be a {error.required_perms} to do that!")
+        return None
+    return print(error)
