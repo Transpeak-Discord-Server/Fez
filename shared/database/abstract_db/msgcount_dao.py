34f393ee0697f3b8ec51aa09efc4594f1df2cac1
Aven <a@avenf.com>
2026-06-10 16:00:52 +0100

    fixed issue with getters

--- shared/database/abstract_db/msgcount_dao.py
+++ shared/database/abstract_db/msgcount_dao.py
@@ -1,6 +1,11 @@
 from abc import ABC, abstractmethod
 
+from sqlalchemy.ext.asyncio import AsyncSession
+
 class MsgCountDAO(ABC):
+
+    def __init__(self, session: AsyncSession):
+        self.session = session
 
     @abstractmethod
     async def increment_message_count(self, user_id: int, amount: int = 1) -> None: ...
--- shared/database/abstract_db/user_dao.py
+++ shared/database/abstract_db/user_dao.py
@@ -1,10 +1,14 @@
 from abc import ABC, abstractmethod
 from typing import Optional
 
+from sqlalchemy.ext.asyncio import AsyncSession
 from shared.database.data import UserData
 
 
 class UserDAO(ABC):
+
+    def __init__(self, session: AsyncSession):
+        self.session = session
 
     @abstractmethod
     async def get_user(self, user_id: int) -> Optional[UserData]: ...
