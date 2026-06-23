c6fe2f65f625c57460322ac2b51190d83fa9b548
Aven <a@avenf.com>
2026-06-23 14:04:46 +0100

    fixed select statements

--- shared/database/old_db/msgcount_dao.py
+++ shared/database/old_db/msgcount_dao.py
@@ -20,7 +20,7 @@ class OldMsgCountDAO(MsgCountDAO):
         await self.session.commit()
 
     async def set_week_count(self, user_id: int, week: int, amount: int) -> None:
-        user_select = select(_MsgcountWeek).where(_MsgcountWeek.id == str(user_id) and _MsgcountWeek.week == str(week))
+        user_select = select(_MsgcountWeek).where(_MsgcountWeek.id == str(user_id)).where(_MsgcountWeek.week == str(week))
         user = (await self.session.scalars(user_select)).first()
         if user is None:
             user = _MsgcountWeek(id=str(user_id), count=str(amount), week=str(week))
@@ -42,7 +42,7 @@ class OldMsgCountDAO(MsgCountDAO):
         return None
 
     async def increment_weekly_count(self, user_id: int, week: int, amount: int = 1) -> None:
-        user_select = select(_MsgcountWeek).where(_MsgcountWeek.id == str(user_id) and _MsgcountWeek.week == str(week))
+        user_select = select(_MsgcountWeek).where(_MsgcountWeek.id == str(user_id)).where(_MsgcountWeek.week == str(week))
         user = (await self.session.scalars(user_select)).first()
         if user is None:
             user = _MsgcountWeek(id=str(user_id), week=str(week), count="0")
@@ -52,7 +52,7 @@ class OldMsgCountDAO(MsgCountDAO):
         return None
 
     async def get_week_messages(self, user_id: int, week: int) -> int:
-        user_select = select(_MsgcountWeek).where(_MsgcountWeek.id == str(user_id) and _MsgcountWeek.week == str(week))
+        user_select = select(_MsgcountWeek).where(_MsgcountWeek.id == str(user_id)).where(_MsgcountWeek.week == str(week))
         user = (await self.session.scalars(user_select)).first()
         if user is None:
             return 0
